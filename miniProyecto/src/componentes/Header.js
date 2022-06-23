8957229eb5e28a6665086c3d9fb4da9ec5a48515
Pabloc43 <97137873+Pabloc43@users.noreply.github.com>
2022-06-23 19:22:35 -0300

    Add files via upload

---
+++ miniProyecto/src/componentes/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-primary py-4">
+        <div className="container">
+            <h1 className="text-light">Practicando useEffect</h1>
+        </div>
+    </header>
+  )
+}
