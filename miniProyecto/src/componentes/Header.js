d8051c3f5c0563220a2c029e6fbd03ce4c74bc5e
Pabloc43 <97137873+Pabloc43@users.noreply.github.com>
2022-06-24 10:25:43 -0300

    Delete miniProyecto directory

--- miniProyecto/src/componentes/Header.js
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-primary py-4">
-        <div className="container">
-            <h1 className="text-light">Practicando useEffect</h1>
-        </div>
-    </header>
-  )
-}
